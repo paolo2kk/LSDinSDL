c7f3b1f740c6fc953463142cbe9dc27ea7cc2a9a
Víctor Cárdenas <victor.cardenas@estudiantat.upc.edu>
2024-10-28 13:13:03 +0100

    ya no te quedas en las esquinas

--- GAME/src/Player.cpp
+++ GAME/src/Player.cpp
@@ -19,21 +19,17 @@ Player::~Player() {
 }
 
 bool Player::Awake() {
-
-	//L03: TODO 2: Initialize Player parameters
 	position = Vector2D(160, 832);
 	return true;
 }
 
 bool Player::Start() {
-	//L03: TODO 2: Initialize Player parameters
 	texture = Engine::GetInstance().textures.get()->Load(parameters.attribute("texture").as_string());
 	position.setX(parameters.attribute("x").as_int());
 	position.setY(parameters.attribute("y").as_int());
 	texW = parameters.attribute("w").as_int();
 	texH = parameters.attribute("h").as_int();
 
-	//Load animations
 	idle.LoadAnimations(parameters.child("animations").child("idle"));
 	currentAnimation = &idle;
 	right.LoadAnimations(parameters.child("animations").child("right"));
@@ -41,17 +37,13 @@ bool Player::Start() {
 	up.LoadAnimations(parameters.child("animations").child("up"));
 	down.LoadAnimations(parameters.child("animations").child("down"));
 
-	// L08 TODO 5: Add physics to the player - initialize physics body
 	pbody = Engine::GetInstance().physics.get()->CreateRectangle((int)position.getX(), (int)position.getY(), texW / 2, texH / 2, bodyType::DYNAMIC);
-	// L08 TODO 6: Assign player class (using "this") to the listener of the pbody. This makes the Physics module to call the OnCollision method
 	pbody->listener = this;
-
-	// L08 TODO 7: Assign collider type
 	pbody->ctype = ColliderType::PLAYER;
 	pbody->body->SetFixedRotation(true);
 	pbody->body->SetGravityScale(0.0f);
 	pbody->body->SetBullet(true);
-	//initialize audio effect
+
 	pickCoinFxId = Engine::GetInstance().audio.get()->LoadFx("Assets/Audio/Fx/retro-video-game-coin-pickup-38299.ogg");
 	velocity.Set(0, 0);
 
@@ -62,69 +54,32 @@ void Player::GravityChange(b2Vec2& velocity)
 {
 	auto input = Engine::GetInstance().input.get();
 
-	if (!canChangeDirection) {
-	
-		return;
-	
+	// Cambia la dirección sin restricciones complejas
+	if (input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+		velocity = b2Vec2(0, GRAVITY_Y);
+		currentAnimation = &up;
+		lastDirection = UP;
 	}
-
-	// Permitir cambio de dirección después de cada frame
-	
-
-	if (canChangeDirection) {
-		if (input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT && lastDirection != UP && currentCollider != ColliderType::UP) {
-			velocity = b2Vec2(0, GRAVITY_Y);
-			currentAnimation = &up;
-			lastDirection = UP;
-			canChangeDirection = false;
-		}
-		else if (input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT && lastDirection != DOWN && currentCollider != ColliderType::DOWN) {
-			velocity = b2Vec2(0, -GRAVITY_Y);
-			currentAnimation = &down;
-			lastDirection = DOWN;
-			canChangeDirection = false;
-		}
-		else if (input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT && lastDirection != LEFT && currentCollider != ColliderType::LEFT) {
-			velocity = b2Vec2(GRAVITY_Y, 0);
-			currentAnimation = &left;
-			lastDirection = LEFT;
-			canChangeDirection = false;
-		}
-		else if (input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT && lastDirection != RIGHT && currentCollider != ColliderType::RIGHT) {
-			velocity = b2Vec2(-GRAVITY_Y, 0);
-			currentAnimation = &right;
-			lastDirection = RIGHT;
-			canChangeDirection = false;
-		}
+	else if (input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) {
+		velocity = b2Vec2(0, -GRAVITY_Y);
+		currentAnimation = &down;
+		lastDirection = DOWN;
+	}
+	else if (input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		velocity = b2Vec2(GRAVITY_Y, 0);
+		currentAnimation = &left;
+		lastDirection = LEFT;
+	}
+	else if (input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		velocity = b2Vec2(-GRAVITY_Y, 0);
+		currentAnimation = &right;
+		lastDirection = RIGHT;
 	}
 }
 
 bool Player::Update(float dt)
 {
-	// L08 TODO 5: Add physics to the player - updated player position using physics
-	//b2Vec2 velocity = b2Vec2(0, pbody->body->GetLinearVelocity().y);
-
 	GravityChange(velocity);
-
-	//// Move left
-	//if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
-	//	velocity.x = -0.2 * dt;
-	//}
-
-	//// Move right
-	//if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
-	//	velocity.x = 0.2 * dt;
-	//}
-	
-	////Jump
-	//if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN && isJumping == false) {
-	//	// Apply an initial upward force
-	//	pbody->body->ApplyLinearImpulseToCenter(b2Vec2(0, -jumpForce), true);
-	//	isJumping = true;
-	//}
-
-	// If the player is jumpling, we don't want to apply gravity, we use the current velocity produced by the jump
-	// Apply the velocity to the player
 	pbody->body->SetLinearVelocity(velocity);
 
 	b2Transform pbodyPos = pbody->body->GetTransform();
@@ -143,17 +98,13 @@ bool Player::CleanUp()
 	return true;
 }
 
-// L08 TODO 6: Define OnCollision function for the player. 
 void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
-	
 	switch (physB->ctype)
 	{
 	case ColliderType::PLATFORM:
 		LOG("Collision PLATFORM");
-		//reset the jump flag when touching the ground
-		canExecuteGrav = true;
+		canChangeDirection = true;
 		isJumping = false;
-
 		break;
 	case ColliderType::ITEM:
 		LOG("Collision ITEM");
@@ -165,32 +116,18 @@ void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
 		LOG("Collision UNKNOWN");
 		canChangeDirection = true;
 		break;
-	case ColliderType::TRIGGER:
-		LOG("Collision TRIGGER");
-		break;
 	case ColliderType::UP:
-		LOG("Collision UP");
-		canChangeDirection = true;
-		currentCollider = ColliderType::UP;
-		break;
 	case ColliderType::DOWN:
-		LOG("Collision DOWN");
-		canChangeDirection = true;
-		currentCollider = ColliderType::DOWN;
-		break;
 	case ColliderType::LEFT:
-		LOG("Collision LEFT");
-		canChangeDirection = true;
-		currentCollider = ColliderType::LEFT;
-		break;
 	case ColliderType::RIGHT:
-		LOG("Collision RIGHT");
+		LOG("Collision with wall");
 		canChangeDirection = true;
-		currentCollider = ColliderType::RIGHT;
+		currentCollider = physB->ctype;
 		break;
 	case ColliderType::ENEMY:
 		LOG("Collided an enemy");
 		isDead = true;
+		break;
 	case ColliderType::BOX:
 		velocity.Set(0, 0);
 		canChangeDirection = true;
@@ -203,34 +140,16 @@ void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
 
 void Player::OnCollisionEnd(PhysBody* physA, PhysBody* physB)
 {
-	static int collisionCount = 0;
 	switch (physB->ctype)
 	{
 	case ColliderType::PLATFORM:
-		LOG("End Collision PLATFORM");
-		
-		break;
 	case ColliderType::ITEM:
-		LOG("End Collision ITEM");
-		velocity.Set(0,0);
-		canChangeDirection = true;
-		lastDirection = NONE;
-		break;
-	
 	case ColliderType::UNKNOWN:
-		LOG("End Collision UNKNOWN");
-		break;
 	case ColliderType::UP:
-		LOG("End Collision UP");
-		break;
 	case ColliderType::DOWN:
-		LOG("End Collision DOWN");
-		break;
 	case ColliderType::LEFT:
-		LOG("End Collision LEFT");
-		break;
 	case ColliderType::RIGHT:
-		LOG("End Collision RIGHT");
+		LOG("End Collision");
 		break;
 	default:
 		break;
