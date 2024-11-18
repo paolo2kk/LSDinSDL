582933b34991ebd6c501ecafae93223f47f145a6
Víctor Cárdenas <victor.cardenas@estudiantat.upc.edu>
2024-11-18 13:12:40 +0100

    murcielago

--- GAME/src/Murcielago.h
+++ GAME/src/Murcielago.h
@@ -1,23 +1,37 @@
-#ifndef MURCIELAGO_H
-#define MURCIELAGO_H
-
-#include "EnemyInClass.h"
+#pragma once
+#include "Entity.h"
+#include "SDL2/SDL.h"
+#include "Animation.h"
 #include "Pathfinding.h"
+#include "EnemyInClass.h"
 #include "Player.h"
 
-class Murcielago : public Enemy {
+class Murcielago : public Entity {
 private:
     Player* player; // Referencia al jugador
-    Pathfinding pathfinding; // Sistema de pathfinding
-    std::vector<Vec2> currentPath; // Camino actual hacia el jugador
+    Pathfinding* pathfinding; // Sistema de pathfinding
+    std::vector<Vector2D> currentPath; // Camino actual hacia el jugador
     int pathIndex; // Índice del nodo actual en el camino
+	int health; // Vida del murciélago
+	int speed; // Velocidad del murciélago
+	SDL_Texture* texture;
+	const char* texturePath;
+	int texW, texH;
+	pugi::xml_node parameters;
+	Animation* currentAnimation = nullptr;
+	Animation idle;
+	PhysBody* pbody;
 
 public:
     Murcielago(Player* target);
     ~Murcielago();
 
-    void Update(float deltaTime) override; // Actualización en cada frame
-    void Draw() override; // Dibuja el murciélago en pantalla
+	bool Awake(); // Inicialización de variables
+	bool Start(); // Inicialización de variables
+    bool Update(float deltaTime); // Actualización en cada frame
+    void Draw(); // Dibuja el murciélago en pantalla
+    void SetPosition(Vector2D pos);
+	Vector2D GetPosition();
 };
 
-#endif // MURCIELAGO_H
+
